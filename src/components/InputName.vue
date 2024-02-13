46fda6b2800a36aa2710fe8bd1eb9465e64a0d2b
Nerdy Gedoni <gedoniani@gmail.com>
2024-02-13 16:10:26 +0100

    router-view added

--- src/components/InputName.vue
+++ src/components/InputName.vue
@@ -1,9 +1,22 @@
+// InputName.vue
 <template>
-  <div>
-     <p class="font-semibold">input name</p>
-  </div>
+  <form @submit.prevent="submitName">
+    <input type="text" v-model="userName" placeholder="Enter your name">
+    <button type="submit">Surprise!</button>
+  </form>
 </template>
 
 <script>
- 
+export default {
+  data() {
+    return {
+      userName: '',
+    };
+  },
+  methods: {
+    submitName() {
+      this.$router.push('/happy-valentines/' + this.userName);
+    },
+  },
+};
 </script>
